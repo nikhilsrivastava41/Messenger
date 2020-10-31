68e525e3cbb0c3862bf59c01f2bd704cba72848f
Nikhil Srivastava <nikhilsrivastava@Nikhils-Air.Dlink>
2020-10-31 13:47:39 +0530

    basic set up done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import React,{ useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput]= useState('');
+  const [messages, setMessages] = useState(["hi","hey"]);
+  console.log(messages);
+  const sendMessage=(event)=>{
+    setMessages([...messages, input]);
+    setInput('');
+  }
   return (
     <div className="App">
-      <h1>HEY</h1>
+      <h1>Chat Karo</h1>
+      <input value={input} onChange={e => setInput(e.target.value)}/>
+      <button onClick={sendMessage}>Send Message</button>
+      {
+        messages.map(msg=>{
+          return <h3>{msg}</h3>
+        })
+      }
     </div>
   );
 }
